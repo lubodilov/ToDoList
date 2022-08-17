1b0902e1788b038022c038f3d629c3feab7cd431
lubod <lubo.daniel.dilov@gmail.com>
2022-08-17 21:03:12 +0300

    added user controller and views

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ToDoList.Models;
+using ToDoList.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,95 @@ namespace ToDoList.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserDTO userDTO)
+        {
+
+            if (ModelState.IsValid)
+            {
+                User user = new User()
+                {
+                    UserName = userDTO.Email,
+                    Email = userDTO.Email
+                };
+
+
+                IdentityResult result = await this.userManager.CreateAsync(user, userDTO.Password);
+
+                if (result.Succeeded)
+                {
+                    TempData["signupDefaultMessage"] = null;
+                    return RedirectToAction(nameof(Login));
+                }
+
+                List<IdentityError> errors = new List<IdentityError>(result.Errors);
+                ViewData["errors"] = errors;
+
+            }
+
+            TempData["signupDefaultMessage"] = false;
+            return View(userDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserDTO userDTO)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Microsoft.AspNetCore.Identity.SignInResult result = await this.signInManager
+                    .PasswordSignInAsync(userDTO.Email,
+                                         userDTO.Password,
+                                         false,
+                                         false);
+
+                if (result.Succeeded)
+                {
+
+                    TempData["loginDefaultMessage"] = null;
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+
+
+            }
+
+            TempData["loginDefaultMessage"] = false;
+            return View(userDTO);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(Index), "Home");
         }
     }
 }
---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,134 @@
+﻿using ToDoList.Models;
+using ToDoList.Models.DTOs;
+using ToDoList.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private IToDoService toDoService;
+        private UserManager<User> userManager;
+        public ToDoController(IToDoService toDoService, UserManager<User> userManager)
+        {
+            this.toDoService = toDoService;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            List<ToDoDTO> toDoes = toDoService.GetAll();
+
+            return View(toDoes);
+        }
+        public async Task<IActionResult> UserToDoesAsync()
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            List<ToDoDTO> toDoes = toDoService.GetUserToDoes(user.Id);
+            return View(toDoes);
+        }
+        public IActionResult Details(int id)
+        {
+            ToDoDTO toDo = toDoService.GetDtoById(id);
+            return View(toDo);
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            ToDo toDo = toDoService.GetById(id);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (toDo.UserId != user.Id)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDoService.GetById(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(int id, ToDo toDo)
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (toDoService.GetById(id).UserId != user.Id)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            toDoService.Edit(toDo);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(ToDo toDo)
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            toDoService.Create(toDo, user);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            ToDo toDo = toDoService.GetById(id);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (toDo.UserId != user.Id)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDo);
+        }
+        public async Task<IActionResult> ConfirmDeleteAsync(int id)
+        {
+            User user = await userManager.GetUserAsync(User).ConfigureAwait(false);
+            ToDo toDo = toDoService.GetById(id);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (toDo.UserId != user.Id)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            toDoService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult ViewToDo(int id)
+        {
+            return View(toDoService.GetById(id));
+        }
+    }
+}
